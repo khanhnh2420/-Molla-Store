6e65935d3116cbf38c0ae883bd8976160fe56422
NguyenHan <handev2607@gmail.com>
2023-03-24 12:02:20 +0700

    Hân
    up code tab Orders đổ dữ liệu bảng orders

--- 4TL_Java6/src/main/resources/static/assets/AngularJS API/MyAccount.js
+++ 4TL_Java6/src/main/resources/static/assets/AngularJS API/MyAccount.js
@@ -1,20 +1,30 @@
 // Angular js
-app.controller("MyAccount", function ($scope, $http, $location) {
-	$scope.form = {}
-    $scope.items = []
-    
-    $scope.getOne = function (username) {
-        var url = `${host}/account/${username}`;
-        $http.get(url).then(resp => {
-            $scope.form = resp.data;
-            console.log("Success", resp);
-        }).catch(error => {
-            console.log("Error", error);
-        });
-    }
-    var param1 = $location.search().username;
-    console.log("abc");
-    console.log(param1);
-    // Lấy ra user có username được truyền vào
-//    $scope.getOne();
+app.controller("MyAccount", function($scope, $http, $location) {
+	$scope.form = {};
+	$scope.orderDetail = [];
+	$scope.totalPrice = 0;
+
+	$scope.showModal = function(orderId) {
+		$('#modalDetailOrder').modal('show');
+		var url = `${host}/orders/${orderId}`;
+		$http.get(url).then(resp => {
+			$scope.orderDetail = resp.data;
+			$scope.totalPrice = 0;
+			for (let i = 0; i < $scope.orderDetail.length; i++) {
+				$scope.totalPrice += $scope.orderDetail[i].price;
+			}
+		}).catch(error => {
+			console.log("Error", error);
+		});
+	}
+
+	// format curency --> VND: 1,234.012
+	$scope.formatCurrency = function(n) {
+		var parts = n.toString().split(".");
+		const numberPart = parts[0];
+		const decimalPart = parts[1];
+		const thousands = /\B(?=(\d{3})+(?!\d))/g;
+		return numberPart.replace(thousands, ".")
+			+ (decimalPart ? "," + decimalPart : "");
+	}
 });
