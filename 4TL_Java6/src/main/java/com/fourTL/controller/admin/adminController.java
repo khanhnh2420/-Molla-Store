82607224abb01631c9a5dc539fb31605f9ae966d
NguyenHan <handev2607@gmail.com>
2023-03-27 19:49:12 +0700

    Hân
    chia layout admin

--- 4TL_Java6/src/main/java/com/fourTL/controller/admin/adminController.java
+++ 4TL_Java6/src/main/java/com/fourTL/controller/admin/adminController.java
@@ -1,5 +1,22 @@
 package com.fourTL.controller.admin;
 
-public class adminController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class adminController {
+	@RequestMapping("/admin/dashboard")
+	public String dashboard() {
+		return "admin/index";
+	}
+	
+	@RequestMapping("/admin/product")
+	public String product() {
+		return "admin/product";
+	}
+	
+	@RequestMapping("/admin/orders")
+	public String orders() {
+		return "admin/orders";
+	}
 }
