114d30f8bc5ce26889872122d5d737b67068d407
NguyenHan <handev2607@gmail.com>
2023-03-31 10:14:20 +0700

    hân
    up code

--- 4TL_Java6/src/main/java/com/fourTL/controller/admin/adminController.java
+++ 4TL_Java6/src/main/java/com/fourTL/controller/admin/adminController.java
@@ -1,15 +1,11 @@
 package com.fourTL.controller.admin;
 
-import java.util.HashMap;
-import java.util.Iterator;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fourTL.dao.CategoriesDAO;
 import com.fourTL.dao.ProductsDAO;
-import com.fourTL.entities.Categories;
 
 @Controller
 public class adminController {
