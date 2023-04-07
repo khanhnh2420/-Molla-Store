274012a87f8fe6217f7c21259e4d045f773d8a1d
huypro-chet <92622056+huypro-chet@users.noreply.github.com>
2023-04-07 20:05:32 +0700

    admin maneger

--- 4TL_Java6/src/main/java/com/fourTL/controller/admin/adminController.java
+++ 4TL_Java6/src/main/java/com/fourTL/controller/admin/adminController.java
@@ -4,13 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fourTL.dao.AccountsDAO;
 import com.fourTL.dao.CategoriesDAO;
+import com.fourTL.dao.OrdersDAO;
 import com.fourTL.dao.ProductsDAO;
 
 @Controller
 public class adminController {
 	@Autowired
 	ProductsDAO productsDAO;
+	
+	@Autowired
+	OrdersDAO ordersDAO;
+	
+	@Autowired
+	AccountsDAO accountsDAO;
 	
 	@Autowired
 	CategoriesDAO categoriesDAO;
@@ -27,5 +35,10 @@ public class adminController {
 	@RequestMapping("/admin/orders")
 	public String orders() {
 		return "admin/orders";
+	}
+	
+	@RequestMapping("/admin/acounts")
+	public String maneger() {
+		return "admin/maneger";
 	}
 }
